6a3cbe306ee98ef240c8ea15ab390d818d6d8955
chris7462 <chris7462@gmail.com>
2024-07-10 22:47:52 -0400

    build-able version

--- include/yolo_object_detection/yolo_object_detection.hpp
+++ include/yolo_object_detection/yolo_object_detection.hpp
@@ -2,16 +2,16 @@
 
 // C++ header
 #include <queue>
-#include <mutex>
-#include <memory>
-#include <vector>
-#include <string>
-#include <filesystem>
+//  #include <mutex>
+//  #include <memory>
+//  #include <vector>
+//  #include <string>
+//  #include <filesystem>
 
-// openCV header
-//#include <opencv2/opencv.hpp>
-#include <opencv2/core.hpp>
-#include <opencv2/dnn.hpp>
+//  // openCV header
+//  //#include <opencv2/opencv.hpp>
+//  #include <opencv2/core.hpp>
+//  #include <opencv2/dnn.hpp>
 
 // ROS header
 #include <rclcpp/rclcpp.hpp>
@@ -22,13 +22,6 @@ namespace yolo_object_detection
 {
 
 namespace fs = std::filesystem;
-
-struct Detection
-{
-  int class_id;
-  float confidence;
-  cv::Rect box;
-};
 
 class YoloObjectDetection : public rclcpp::Node
 {
@@ -48,13 +41,13 @@ private:
 
   std::mutex mtx_;
 
-  bool load_classes(fs::path class_file);
-  void load_net(fs::path model_file);
-  cv::Mat format_yolov5(const cv::Mat & source);
-  void detect(cv::Mat & image, std::vector<Detection> & output);
+//  bool load_classes(fs::path class_file);
+//  void load_net(fs::path model_file);
+//  cv::Mat format_yolov5(const cv::Mat & source);
+//  void detect(cv::Mat & image, std::vector<Detection> & output);
 
-  std::vector<std::string> class_list_;
-  cv::dnn::Net net_;
+//  std::vector<std::string> class_list_;
+//  cv::dnn::Net net_;
 };
 
 } // namespace yolo_object_detection
