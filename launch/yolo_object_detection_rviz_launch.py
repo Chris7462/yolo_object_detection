b28f489e3fc8030416c84b0086765db5da30fb92
Chris7462 <chris7462@gmail.com>
2026-07-02 13:15:53 -0400

    bump to lyrical

--- launch/yolo_object_detection_rviz_launch.py
+++ launch/yolo_object_detection_rviz_launch.py
@@ -3,44 +3,54 @@ from os.path import join
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import ExecuteProcess, IncludeLaunchDescription, TimerAction
+from launch.actions import (DeclareLaunchArgument, ExecuteProcess,
+                            IncludeLaunchDescription, TimerAction)
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution
-from launch_ros.actions import Node, SetParameter
-from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    pkg_share = get_package_share_directory('yolo_object_detection')
+
+    declare_use_sim_time = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',
+        description='Use simulation (bagfile) clock if true'
+    )
+
     bag_exec = ExecuteProcess(
         cmd=['ros2', 'bag', 'play', '-r', '1.0',
-             '/data/kitti/raw/2011_09_29_drive_0071_sync_bag', '--clock']
+             '/data/kitti/raw/2011_09_29_drive_0071_sync_bag',
+             '--clock',
+             '--qos-profile-overrides-path',
+             join(pkg_share, 'config', 'qos_override_offline.yaml')]
     )
 
     yolo_object_detection_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource([
-            PathJoinSubstitution([
-                FindPackageShare('yolo_object_detection'), 'launch',
-                'yolo_object_detection_launch.py'
-            ])
-        ])
+        PythonLaunchDescriptionSource(
+            join(pkg_share, 'launch', 'yolo_object_detection_launch.py')
+        ),
+        launch_arguments={
+            'use_sim_time': LaunchConfiguration('use_sim_time')
+        }.items()
     )
 
     rviz_node = Node(
         package='rviz2',
         executable='rviz2',
         name='rviz2',
-        arguments=['-d', join(get_package_share_directory('yolo_object_detection'),
-                              'rviz', 'yolo_object_detection.rviz')]
+        arguments=['-d', join(pkg_share, 'rviz', 'yolo_object_detection.rviz')]
     )
 
     return LaunchDescription([
-        SetParameter(name='use_sim_time', value=True),
-        bag_exec,
+        declare_use_sim_time,
+        yolo_object_detection_launch,
         rviz_node,
         TimerAction(
-            period=1.0,  # delay these nodes for 1.0 seconds.
+            period=3.0,  # delay these nodes for 3.0 seconds.
             actions=[
-                yolo_object_detection_launch
+                bag_exec
             ]
         )
     ])
